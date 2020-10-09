eb22c293bd316039587db0905be39c1b8197bb81
Joe Alves <jmalves@appacademy.io>
2020-10-08 17:02:51 -0700

    End of End of Day

--- Clock.js
+++ Clock.js
@@ -1,9 +1,45 @@
+import {
+    createNewClockHTML,
+    setTimeOnClock,
+    getCurrentTimeInTimezone,
+    wallElement
+} from "/do-not-edit/clockDOMUtilities.js";
+
 export class Clock {
     constructor(region, timezone) {
-        
+        this.region = region;
+        this.timezone = timezone;
+        this.element = document.createElement("div");
+        this.element.innerHTML = createNewClockHTML(this.region);
+    }
+    hangOnWall() {
+        wallElement.appendChild(this.element);
+    }
+    takeOffWall() {
+        this.element.remove();
+    }
+    setTime() {
+        setTimeOnClock(
+            this.element, 
+            getCurrentTimeInTimezone(this.timezone)
+        );
     }
 }
 
-export class LiveClock {
-
+export class LiveClock extends Clock {
+    constructor(region, timezone) {
+        super(region, timezone);
+        this.start();
+    }
+    start() {
+        this.runningInterval = setInterval(() => {
+            this.setTime();
+        }, 1000);
+    }
+    stop() {
+        clearInterval(this.runningInterval);
+    }
+    resume() {
+        this.start();
+    }
 }
--- program.js
+++ program.js
@@ -5,68 +5,78 @@ import {
   getCurrentTimeInTimezone,
 } from "/do-not-edit/clockDOMUtilities.js";
 
-// import {Clock, LiveClock} from "/Clock.js";
+import {Clock, LiveClock} from "/Clock.js";
 
-const eastCoastClock = document.createElement("div");
-eastCoastClock.innerHTML = createNewClockHTML("The Big Apple");
-setTimeOnClock(eastCoastClock, getCurrentTimeInTimezone("America/New_York"));
-wallElement.appendChild(eastCoastClock);
-setTimeout(() => {
-  eastCoastClock.remove();
-}, 15000);
-/*
+// const eastCoastClock = document.createElement("div");
+// eastCoastClock.innerHTML = createNewClockHTML("The Big Apple");
+// setTimeOnClock(eastCoastClock, getCurrentTimeInTimezone("America/New_York"));
+// wallElement.appendChild(eastCoastClock);
+// setTimeout(() => {
+//   eastCoastClock.remove();
+// }, 15000);
+
 const eastCoast = new Clock(
     "The Big Apple",
     "America/New_York"
 );
-eastCoast.hangOnWall();
+// eastCoast.hangOnWall();
+eastCoast.setTime();
 setTimeout(() => {
     eastCoast.takeOffWall();
 }, 10000);
-*/
 
-const westCoastClock = document.createElement("div");
-westCoastClock.innerHTML = createNewClockHTML("a/A HQ");
-setTimeOnClock(westCoastClock, getCurrentTimeInTimezone("America/Los_Angeles"));
-wallElement.appendChild(westCoastClock);
+// const westCoastClock = document.createElement("div");
+// westCoastClock.innerHTML = createNewClockHTML("a/A HQ");
+// setTimeOnClock(westCoastClock, getCurrentTimeInTimezone("America/Los_Angeles"));
+// wallElement.appendChild(westCoastClock);
 
-/*
+
 const westCoast = new LiveClock(
     "a/A HQ",
     "America/Los_Angeles"
 );
+westCoast.setTime();
 westCoast.hangOnWall();
-*/
 
-let clockRunningInterval = setInterval(() => {
-  setTimeOnClock(
-    westCoastClock,
-    getCurrentTimeInTimezone("America/Los_Angeles")
-  );
-}, 1000);
+console.log(westCoast);
+
+
+// let clockRunningInterval = setInterval(() => {
+//   setTimeOnClock(
+//     westCoastClock,
+//     getCurrentTimeInTimezone("America/Los_Angeles")
+//   );
+// }, 1000);
+
+// setTimeout(() => {
+//   clearInterval(clockRunningInterval);
+// }, 5000);
+
+// setTimeout(() => {
+//   clockRunningInterval = setInterval(() => {
+//     setTimeOnClock(
+//       westCoastClock,
+//       getCurrentTimeInTimezone("America/Los_Angeles")
+//     );
+//   }, 1000);
+// }, 10000);
+
+// setTimeout(() => {
+//   westCoastClock.remove();
+//   clearInterval(clockRunningInterval);
+// }, 15000);
+
+
 setTimeout(() => {
-  clearInterval(clockRunningInterval);
-}, 5000);
-setTimeout(() => {
-  clockRunningInterval = setInterval(() => {
-    setTimeOnClock(
-      westCoastClock,
-      getCurrentTimeInTimezone("America/Los_Angeles")
-    );
-  }, 1000);
-}, 10000);
-setTimeout(() => {
-  westCoastClock.remove();
-  clearInterval(clockRunningInterval);
-}, 15000);
-/* 
-setTimeout(() => {
+    console.log("calling stop");
     westCoast.stop();
 }, 5000);
+
 setTimeout(() => {
     westCoast.resume();
 }, 10000);
+
 setTimeout(() => {
     westCoast.takeOffWall();
 }, 15000);
-*/
+
